c2bc647243a433fc9682c9a2b7a28d1390e25cc6
thualinhtutxero <thuralinhtut3@gmail.com>
2022-10-09 17:39:06 +0630

    endtoday

--- Database.js
+++ Database.js
@@ -122,7 +122,7 @@ export const isArrayhasData = (arr = []) => {
   return arr.length >= 1;
 };
 
-// export const baseUrl = 'https://empirepos.pythonanywhere.com';
+export const baseUrl = 'https://empirepos.pythonanywhere.com';
 
-export const baseUrl = 'http://192.168.43.247:8000';
+// export const baseUrl = 'http://192.168.43.247:8000';
 export const appversion = '1.0';
