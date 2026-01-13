4fc0ba513511347202d446d89757694b967d9830
empiretylh <thuralinhtut3@gmail.com>
2026-01-13 21:48:08 +0630

    export for walpos

--- Database.js
+++ Database.js
@@ -139,8 +139,8 @@ export const isArrayhasData = (arr = []) => {
 };
 
 // export const baseUrl = 'https://empirepos.pythonanywhere.com'; //without image
-export const baseUrl = 'https://mttonlinepos.pythonanywhere.com'; //with image
-// export const baseUrl = 'https://walposapi.mml.com.mm';
+// export const baseUrl = 'https://mttonlinepos.pythonanywhere.com'; //with image
+export const baseUrl = 'https://walposapi.mml.com.mm';
 
 
 // export const baseUrl = 'http://192.168.100.63:8000';  
